5ec775be7777341ef0aa928826c9a6d35742d1fd
mahamudhasan0 <mahmudhasan960458@gmail.com>
2021-02-12 18:11:27 +0600

    salary app

---
+++ salaryData.js
@@ -0,0 +1,64 @@
+var salary_data = {
+  "0" : {
+    "hobby" : "talking javascript",
+    "name" : "AB Khan",
+    "salary" : "76340.0"
+  },
+  "1" : {
+    "name" : "Paul Irish",
+    "salary" : "94320.0",
+    "status" : "married"
+  },
+  "2" : {
+    "name" : "Devid Fist",
+    "salary" : "67000.0"
+  },
+  "3" : {
+    "name" : "Pete sampras",
+    "salary" : "105540.0"
+  },
+  "4" : {
+    "name" : "Remy Sharp",
+    "salary" : "82220.0"
+  },
+  "5" : {
+    "name" : "David Beckham",
+    "salary" : "64430.0",
+    "sleepHour" : 4
+  },
+  "6" : {
+    "favLibrary" : "React",
+    "name" : "A Random Guy",
+    "pizzaSlice" : 5,
+    "salary" : "36000.0"
+  },
+  "7" : {
+    "name" : "JS Dude",
+    "salary" : "44432"
+  },
+  "-JvacZOcfUEvub7mxeo-" : {
+    "name" : "Karim",
+    "organization" : "Nielsen",
+    "salary" : "53454"
+  },
+  "-Jvac_s61TL9oYM5SjMR" : {
+    "name" : "Jamal",
+    "salary" : "43454"
+  },
+  "-JvacbMTxlHCG5VaVDWg" : {
+    "name" : "Sheikh",
+    "salary" : "33454"
+  },
+  "-JviNyklLgZqk4rSB8pA" : {
+    "name" : "Tom Cruise",
+    "salary" : "23444"
+  },
+  "-JviO0Y_tNNMQNrdZVBR" : {
+    "name" : "Bill",
+    "salary" : "63444"
+  },
+  "-JviO3gXUpSdnZHtliWg" : {
+    "name" : "Steve",
+    "salary" : "53655"
+  }
+}
